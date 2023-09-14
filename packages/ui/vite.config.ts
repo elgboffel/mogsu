fb280e8c47405049802987381ba9f1a798702abe
jok <jok@kruso.dk>
2023-09-14 11:13:22 +0200

    Remove pnpm-lock file from packages/ui

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -8,13 +8,13 @@ export default defineConfig(() => {
     build: {
       target: "es2020",
       lib: {
-        entry: "./src/index.ts",
+        entry: "./src/index.tsx",
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
       },
       rollupOptions: {
         // externalize deps that shouldn't be bundled into the library
-        external: [/^node:.*/],
+        external: [/^node:.*/, "@project/styled-system"],
       },
     },
   };
